6f25c49ad8f10cd6f17f6b030f4338c9615201d3
奥田啓皓 <>
2022-06-17 19:18:12 +0900

    storiesファイルの使用していないreactインポートを削除

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -8,7 +8,7 @@ interface item {
 }
 const Item: React.VFC<item> = (props) => {
   return (
-    <div className="h-[300px] h-full w-[calc((100%-7rem)/3)] shrink-0 cursor-pointer snap-start snap-start rounded-lg border border-2 p-6 shadow-lg transition-all ease-out first:ml-6 last:mr-6 hover:scale-110 hover:border-4 hover:shadow-xl">
+    <div className="h-[300px] w-[calc((100%-7rem)/3)] shrink-0 cursor-pointer snap-start snap-start rounded-lg border border-2 p-6 shadow-lg transition-all ease-out first:ml-6 last:mr-6 hover:scale-110 hover:border-4 hover:shadow-xl">
       <div className="w-100 flex h-full flex-col justify-between">
         <img src={props.item.imgUrl} alt="" className="mx-auto h-3/4" />
         <div>
